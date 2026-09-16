3a1531884912256fd696eb2e37292408625c0bc6
cramp05 <192006155+cramp05@users.noreply.github.com>
2026-09-16 10:25:46 +0200

    fffff

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem; //para usar el input action
 
 public class PlayerControler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private int _maxHealth = 100;
+
+    [SerializeField] private float _movementSpeed = 4.5f;
+
+    //float "nombre" = 5.7f;  con decimales
+    //bool "nombre" = "true/false";
+    //string  texto
+    //int   numero entero
+
+    private Rigidbody2D _rigidbody2D;
+
+    private InputAction _moveAction;
+    private Vector2 _moveInput;
+
+    void Awake() 
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        _moveAction = InputSystem.actions["Move"];
+    }
+
+
     void Start()
     {
         
@@ -11,6 +34,14 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _moveInput = _moveAction.ReadValue<Vector2>(); //leemos el valor de las teclas pulsadas del move
+
         
     }
+
+    void FixedUpdate()
+    {
+        _rigidbody2D.linearVelocity = new Vector2(_moveInput.x *_movementSpeed, _rigidbody2D.linearVelocity.y); //Mover personaje
+    }
+
 }
