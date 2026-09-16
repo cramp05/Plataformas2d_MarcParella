fbcb87a22700b8aca7a4eeec38f72b462bae14c9
cramp05 <192006155+cramp05@users.noreply.github.com>
2026-09-16 10:58:30 +0200

    dwadwad

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -7,6 +7,7 @@ public class PlayerControler : MonoBehaviour
     private int _maxHealth = 100;
 
     [SerializeField] private float _movementSpeed = 4.5f;
+    [SerializeField] private float _forceJump = 10;
 
     //float "nombre" = 5.7f;  con decimales
     //bool "nombre" = "true/false";
@@ -16,13 +17,18 @@ public class PlayerControler : MonoBehaviour
     private Rigidbody2D _rigidbody2D;
 
     private InputAction _moveAction;
+    private InputAction _jumpAction;
     private Vector2 _moveInput;
+    
+
+
 
     void Awake() 
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
 
         _moveAction = InputSystem.actions["Move"];
+        _jumpAction = InputSystem.actions["Jump"];
     }
 
 
@@ -36,12 +42,31 @@ public class PlayerControler : MonoBehaviour
     {
         _moveInput = _moveAction.ReadValue<Vector2>(); //leemos el valor de las teclas pulsadas del move
 
+        if(_moveInput.x < 0) //para rotar el personaje al cambiar la direccion
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if(_moveInput.x > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if(_jumpAction.WasPressedThisFrame()) //para llamar la funcion de salto al pulsar tecla
+        {
+            Jump();
+        }
+
         
     }
 
     void FixedUpdate()
     {
         _rigidbody2D.linearVelocity = new Vector2(_moveInput.x *_movementSpeed, _rigidbody2D.linearVelocity.y); //Mover personaje
+    }
+
+    void Jump() //crear el movimiento de salto
+    {
+        _rigidbody2D.AddForce(Vector2.up, ForceMode2D.Impulse);
     }
 
 }
